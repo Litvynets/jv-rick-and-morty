f75b5a99930d9d4f09c2862a01a9f0ce0b14dfa9
Ivan Litvynets <Litvyvanya@gmail.com>
2024-10-01 23:17:48 +0200

    CustomGlobalExceptionHandler annotated

--- src/main/java/mate/academy/rickandmorty/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/mate/academy/rickandmorty/exception/CustomGlobalExceptionHandler.java
@@ -5,9 +5,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
     private static final String TIMESTAMP = "timestamp";
     private static final String STATUS = "status";
